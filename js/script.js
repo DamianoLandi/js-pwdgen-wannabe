c982938a874e8025db73282fab7207f432f891c4
DamianoLandi <damianolandi01@gmail.com>
2021-06-30 16:19:31 +0200

    Collegamento Elementi HTML

--- js/script.js
+++ js/script.js
@@ -8,6 +8,20 @@ var cognome = (prompt("Quale è il tuo cognome?"));
 
 var colore = (prompt("Quale è il tuo colore preferito?"));
 
+var password = nome + cognome + colore + "21";
+
 console.log(nome);
 console.log(cognome);
 console.log(colore);
+console.log(password);
+
+//Collegamento Elementi HTML
+
+var name = document.getElementById("name");
+
+var surname = document.getElementById("surname");
+
+var color = document.getElementById("color");
+
+var pw = document.getElementById("pw");
+
