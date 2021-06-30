d9ccf74f97a295f6414da3bca2d6fb7e6d504a26
DamianoLandi <damianolandi01@gmail.com>
2021-06-30 16:10:18 +0200

    Raccolta Variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("OK")
+console.log("OK");
+
+//Raccolta Variabili
+
+var nome = (prompt("Come ti chiami?"));
+
+var cognome = (prompt("Quale è il tuo cognome?"));
+
+var colore = (prompt("Quale è il tuo colore preferito?"));
+
+console.log(nome);
+console.log(cognome);
+console.log(colore);
